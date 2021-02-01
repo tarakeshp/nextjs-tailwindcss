d3628273d637865f69ce8c78d348d499cbfda900
Tarakesh Pulikonda <tarakesh.p@gmail.com>
2021-02-01 23:25:51 +0530

    first commit

---
+++ pages/login.js
@@ -0,0 +1,6 @@
+import Head from 'next/head';
+export default function Login() {
+    return (
+        <div>This is a login page</div>
+    );
+}
---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+
+// If you want to use other PostCSS plugins, see the following:
+// https://tailwindcss.com/docs/using-with-preprocessors
+// module.exports = {
+//     plugins: {
+//         tailwindcss: {},
+//         autoprefixer: {},
+//     },
+// }
+
+module.exports = {
+    plugins: ['tailwindcss', 'postcss-preset-env'],
+}
